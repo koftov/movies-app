323b3d80bcb56268f33406b996ccfe87512e8918
koftov <ahronlu@gmail.com>
2020-07-07 14:54:39 +0300

    Updates

---
+++ precache-manifest.1bb31c8f6cd574a4f6f7d71e24af1f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aae66c54fd5c60d41631",
+    "url": "/movies-app/static/css/main.1278c0e2.chunk.css"
+  },
+  {
+    "revision": "aae66c54fd5c60d41631",
+    "url": "/movies-app/static/js/main.aae66c54.chunk.js"
+  },
+  {
+    "revision": "87f7407a67ef306dbf94",
+    "url": "/movies-app/static/js/1.87f7407a.chunk.js"
+  },
+  {
+    "revision": "267837c2ea3a538e1c74",
+    "url": "/movies-app/static/js/runtime~main.267837c2.js"
+  },
+  {
+    "revision": "3203e6ed20b7f12bbac8a3aff3f9504f",
+    "url": "/movies-app/index.html"
+  }
+];
